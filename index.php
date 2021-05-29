2255b6dfc64405460bf150c15a68a528e9e7a413
mailinnence <78959189+mailinnence@users.noreply.github.com>
2021-05-29 23:36:07 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<html>
+<head>
+    <style>
+        form{
+         text-align: center;
+         position: relative;
+         top:250px;
+
+        }
+    </style>
+</head>
+<body>
+
+<form name="k" action="main.php" method="post">
+    <p>비밀번호를 눌러 주세요</p>
+    <input type="text" name="pw"><br><br>
+    <input type="submit" value="확인">
+</form>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+if($_POST["pw"]=="Sharon"){
+    echo "<script>location.href='main2.php';</script>";
+}
+else
+    echo "<script>location.href='index.php';</script>";
+
+?>
